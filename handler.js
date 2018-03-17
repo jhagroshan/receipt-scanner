7bfb10b82f21915d9cc6dc742b58c32709570251
jsin68 <jsin68@student.monash.edu>
2018-03-17 20:20:37 +1100

    rewrote codebase

--- handler.js
+++ handler.js
@@ -1,16 +1,35 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  var receipt = require('./receipt.js');
 
-  callback(null, response);
+  if (event.queryStringParameters && event.queryStringParameters.img && event.queryStringParameters.bucket) {
+    var bucket = event.queryStringParameters.bucket;
+    var img = event.queryStringParameters.img;
+    
+    receipt.analyze(bucket, img, function(result){
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: result
+        })
+      };
+      return callback(null, response)
+    }); 
+  }
+  else {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "invalid input",
+        input: event,
+      })
+    };
 
+    callback(null, response)
+  }
+
+  
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
